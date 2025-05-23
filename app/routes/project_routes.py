d26410ada139202eaad241f980519a56129bf213
jamesonyemi <jamesoneyemi@gmail.com>
2025-05-23 16:27:42 +0000

    ui changes

--- app/routes/project_routes.py
+++ app/routes/project_routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from flask import render_template
 
-project_bp = Blueprint('project', __name__)
+project_bp = Blueprint('project', __name__, url_prefix='/projects', template_folder='templates')
 
 @project_bp.route('/')
 def index():
-    return '<h1>Project routes</h1>'
+    page_title = 'Projects'
+    return render_template('pages/projects/index.html', page_title=page_title)
+
